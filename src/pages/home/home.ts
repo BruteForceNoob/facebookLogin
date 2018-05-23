7ee7f942ed794661fee4b60b2ecbf2a915aad547
BruteForceNoob <panicker.sujeeth@gmail.com>
2018-05-23 12:26:32 -0700

    Ionic native facebook plugin implementation
    This is a simple test application to let users login through their facebook credentials.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,58 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import { Facebook, FacebookLoginResponse } from '@ionic-native/facebook';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public fb: Facebook) {
 
   }
 
+  loginAction()
+{
+    // permissions
+    this.fb.login(['public_profile', 'user_photos', 'email', 'user_birthday'])
+    .then( (res: FacebookLoginResponse) => {
+
+        
+        if(res.status == "connected") {
+
+            // Get user ID and Token
+            var fb_id = res.authResponse.userID;
+            var fb_token = res.authResponse.accessToken;
+
+            // Get user infos from the API
+            this.fb.api("/me?fields=name,gender,birthday,email", []).then((user) => {
+
+                // Get the connected user details
+                var gender    = user.gender;
+                var birthday  = user.birthday;
+                var name      = user.name;
+                var email     = user.email;
+
+                console.log("=== USER INFOS ===");
+                console.log("Gender : " + gender);
+                console.log("Birthday : " + birthday);
+                console.log("Name : " + name);
+                console.log("Email : " + email);
+
+                
+            });
+
+        } 
+        
+        else {
+			
+            console.log("An error occurred while logging in!");
+
+        }
+
+    })
+    .catch((e) => {
+        console.log('Error logging into Facebook', e);
+    });
+}
 }
